d30e3f36d6b41a3d73c50054400d74055269c28c
Suhas <saisuhas.1324@gmail.com>
2023-08-24 14:38:46 +0530

    delete function complete

--- exp_tracker/home/views.py
+++ exp_tracker/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from . import models
-from .models import Expense
+from .models import Expense, Profile
 import csv
 
 # * Import required modules and functions for rendering views and interacting with models.
@@ -67,19 +67,21 @@ def home(request):
 def delete_expense(request, expense_id):
     expense = get_object_or_404(Expense, id=expense_id, user=request.user)
     
+    profile = Profile.objects.filter(user=request.user).first()
+    
     if request.method == 'POST':
-        if 'expenseName' in request.POST and 'expenseAmount' in request.POST:
-            expense_name = request.POST['expenseName']
-            expense_amount = float(request.POST['expenseAmount'])
-            
-            if expense.expense_type == 'income':
-                request.user.profile.income -= expense_amount
-            else:
-                request.user.profile.expenses -= expense_amount
-            
-            request.user.profile.balance += expense_amount
-            request.user.profile.save()
-            
+        expense_amount = expense.amount
+        
+        if expense.expense_type == 'income':
+            profile.income -= expense_amount
+            profile.balance -= expense_amount
+        else:
+            profile.expenses -= expense_amount
+            profile.balance -= expense_amount
+        
+        profile.save()
+        
         expense.delete()
     
     return redirect('/home/')  # Redirect back to the home page
+
